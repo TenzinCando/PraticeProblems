72518fb5511dcfe16bc6d17603a541299b94d24e
TenzinCando <tenzink@uw.edu>
2019-06-01 15:16:24 -0700

    adding pay type name on tooltip

--- InterviewProblems/DataVisualization/d3config.js
+++ InterviewProblems/DataVisualization/d3config.js
@@ -9,7 +9,10 @@ let parse = d3.time.format("%Y-%m-%d").parse;
 // Transpose the data into layers
 let dataset = d3.layout.stack()( payTypesIds.map(function(payType) {
   return data.map(function(d) {
-    return {x: parse(d.date), y: ((payType in d) ? +d[payType] : 0 ) };
+    // Shorten the name of the pay type
+    let payTypeName = payTypesIdsMap[payType];
+    payTypeName = ( payTypeName.length > 9 ? `${payTypeName.substring(0,10)}...` : payTypeName);
+    return {x: parse(d.date), y: ((payType in d) ? +d[payType] : 0 ), label: payTypeName };
   });
 }));
 
@@ -64,10 +67,14 @@ let rect = groups.selectAll("rect")
   .on("mouseover", function() { tooltip.style("display", null); })
   .on("mouseout", function() { tooltip.style("display", "none"); })
   .on("mousemove", function(d) {
+      console.log('moving mouse');
     let xPosition = d3.mouse(this)[0] - 15;
     let yPosition = d3.mouse(this)[1] - 25;
     tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-    tooltip.select("text").text('$' + d.y);
+    tooltip.select("text").html( `${d.label}<br>$${d.y}`);
+    tooltip.selectAll("text").text(function(i, t) {
+        return (t == 1) ? `$${d.y}` : d.label;
+    });
   });
 
 
@@ -100,14 +107,22 @@ let tooltip = svg.append("g")
   .style("display", "none");
     
 tooltip.append("rect")
-  .attr("width", 60)
-  .attr("height", 20)
+  .attr("width", 100)
+  .attr("height", 35)
   .attr("fill", "black")
   .style("opacity", 0.8);
 
 tooltip.append("text")
-  .attr("x", 30)
+  .attr("x", 50)
   .attr("dy", "1.2em")
+  .style("text-anchor", "middle")
+  .attr("font-size", "12px")
+  .attr("font-weight", "bold")
+  .attr("fill", "white");
+
+ tooltip.append("text")
+  .attr("x", 50)
+  .attr("dy", "2.3em")
   .style("text-anchor", "middle")
   .attr("font-size", "12px")
   .attr("font-weight", "bold")
